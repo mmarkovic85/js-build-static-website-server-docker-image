53ac4d2e937e118f885f200466d45b8430adbc5b
Miloš Marković <milosmarkovic85@gmail.com>
2023-05-12 23:19:11 +0200

    server app: use env PORT, path.resolve public dir

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+const path = require('path');
 const express = require('express');
 
-//
 const app = express();
 
 // config
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // serve static files
-app.use(express.static('public'));
+app.use(express.static(path.resolve('public')));
 
 // start server
 app.listen(port, () => {
